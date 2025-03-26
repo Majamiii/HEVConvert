a6eb8c47ff58f1726fb0fef2b45882f9c6672907
Majamiii <maja.milovic05@gmail.com>
2025-03-27 00:01:41 +0100

    h265 video compression

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,17 @@ use std::process::Command;
 
 fn main() {
     
+    let input_path = r"C:\Users\Administrator\Downloads\video.mp4";
+    let output_path = r"C:\Users\Administrator\Downloads\compressed-video-x265.mp4";
 
-    let output = Command::new("cmd")
-    .args(&["/C", "ffmpeg -i C:/Users/Administrator/Downloads/zana-slika.jpg C:/Users/Administrator/Downloads/zana-slika4.bmp"])
+    let output = Command::new("ffmpeg")
+    .args(&["-y", "-i", input_path, 
+    "-c:v", "libx265",  // video codec (h265)
+    "-crf", "25",       // constant rate factor - video quality - the smaller the better quality (24-28 is a good balance)
+    "-preset", "slow",  //encoding speed
+    "-c:a", "aac",      // audio codec
+    "-b:a", "128k",     // bitrate
+    output_path])
     .output()
     .expect("failed to convert");
 
