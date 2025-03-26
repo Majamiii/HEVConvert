270755997d340e8890f7e2a5587aba6509a6d9f7
Majamiii <maja.milovic05@gmail.com>
2025-03-26 22:41:30 +0100

    converting 1 file with ffmpeg

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+// use std::path::Path;
+use std::process::Command;
+// use std::ffi::OsStr;
+// use std::path::PathBuf;
+
 fn main() {
-    println!("Hello, world!");
+    
+
+    let output = Command::new("cmd")
+    .args(&["/C", "ffmpeg -i C:/Users/Administrator/Downloads/zana-slika.jpg C:/Users/Administrator/Downloads/zana-slika4.bmp"])
+    .output()
+    .expect("failed to convert");
+
+    println!("Status: {}", output.status);
+    println!("Stdout: {}", String::from_utf8_lossy(&output.stdout));
+    println!("Stderr: {}", String::from_utf8_lossy(&output.stderr));
+
+    return;
 }
